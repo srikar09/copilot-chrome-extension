42241c30e22ac49cc6e0d5030bc88eeed4fb0d22
yoan <yoanyombapro@gmail.com>
2023-07-30 17:55:44 -0400

    feat: update financial portal
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- src/pages/authentication/login.tsx
+++ src/pages/authentication/login.tsx
@@ -5,7 +5,7 @@ import { Layout } from "src/layouts/layout";
 const LoginPage: React.FC = () => {
   return (
     <Layout>
-      <div className="pt-[20%] sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="pt-[5%] sm:mx-auto sm:w-full sm:max-w-md">
         <RocketIcon className="mx-auto h-10 w-auto" />
         <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
