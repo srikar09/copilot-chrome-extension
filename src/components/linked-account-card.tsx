0a6591f47e7db83ccb3ab59b4032a8da8650924b
yoan <yoanyombapro@gmail.com>
2023-08-02 23:10:41 -0400

    feat: financial context
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- src/components/linked-account-card.tsx
+++ src/components/linked-account-card.tsx
@@ -39,7 +39,7 @@ const LinkedAccountCard: React.FC<{
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">${totalBalance}</div>
-        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+        {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
       </CardContent>
     </Card>
   );
