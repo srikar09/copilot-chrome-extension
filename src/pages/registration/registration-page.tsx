9ab66d57bfb223aae7ed0865f0b5a78376ae83f8
yoan <yoanyombapro@gmail.com>
2023-08-10 17:17:49 -0400

    feat: finish registration
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- src/pages/registration/registration-page.tsx
+++ src/pages/registration/registration-page.tsx
@@ -34,7 +34,7 @@ export default function RegistrationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-2">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
