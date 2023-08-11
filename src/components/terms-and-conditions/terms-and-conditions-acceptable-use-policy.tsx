e004d8e142656d0bbb1963c258a25425698ab446
Simfinii_admin <82017432+SimifiniiCTO@users.noreply.github.com>
2023-08-11 13:56:16 -0400

    privacy policy

--- src/components/terms-and-conditions/terms-and-conditions-acceptable-use-policy.tsx
+++ src/components/terms-and-conditions/terms-and-conditions-acceptable-use-policy.tsx
@@ -11,8 +11,7 @@ const AcceptableUsePolicy: React.FC = () => {
 # Acceptable Use Policy
 ## 1. Overview
 
-Infosec’s intentions for publishing an Acceptable Use Policy are not to impose restrictions that are contrary to melodiy
-LLC’s established culture of openness, trust and integrity. Infosec is committed to protecting melodiy LLC's employees,
+Infosec’s intentions for publishing an Acceptable Use Policy are not to impose restrictions that are contrary to simfiny, Inc's established culture of openness, trust and integrity. Infosec is committed to protecting simfiny, Inc's employees,
 partners and the company from illegal or damaging actions by individuals, either knowingly or unknowingly.
 
 Internet/Intranet/Extranet-related systems, including but not limited to computer equipment, software, operating
@@ -20,40 +19,40 @@ systems, storage media, network accounts providing electronic mail, WWW browsing
 LLC. These systems are to be used for business purposes in serving the interests of the company, and of our clients and
 customers in the course of normal operations. Please review Human Resources policies for further details.
 
-Effective security is a team effort involving the participation and support of every melodiy LLC employee and
+Effective security is a team effort involving the participation and support of every simfiny, Inc employee and
 affiliate who deals with information and/or information systems. It is the responsibility of every computer user to know
 these guidelines, and to conduct their activities accordingly.
 
 ## 2. Purpose
 
-The purpose of this policy is to outline the acceptable use of computer equipment at melodiy LLC. These rules are in
-place to protect the employee and melodiy LLC. Inappropriate use exposes melodiy LLC to risks including virus
+The purpose of this policy is to outline the acceptable use of computer equipment at simfiny, Inc. These rules are in
+place to protect the employee and simfiny, Inc. Inappropriate use exposes simfiny, Inc to risks including virus
 attacks, compromise of network systems and services, and legal issues.
 
 ## 3. Scope
 
 This policy applies to the use of information, electronic and computing devices, and network resources to conduct melodiy
-LLC business or interact with internal networks and business systems, whether owned or leased by melodiy LLC, the
-employee, or a third party. All employees, contractors, consultants, temporary, and other workers at melodiy LLC and its
+LLC business or interact with internal networks and business systems, whether owned or leased by simfiny, Inc, the
+employee, or a third party. All employees, contractors, consultants, temporary, and other workers at simfiny, Inc and its
 subsidiaries are responsible for exercising good judgment regarding appropriate use of information, electronic devices,
-and network resources in accordance with melodiy LLC policies and standards, and local laws and regulation. Exceptions to
+and network resources in accordance with simfiny, Inc policies and standards, and local laws and regulation. Exceptions to
 this policy are documented in [section 5.2](#52-exceptions)
 
-This policy applies to employees, contractors, consultants, temporaries, and other workers at melodiy LLC, including all
-personnel affiliated with third parties. This policy applies to all equipment that is owned or leased by melodiy LLC.
+This policy applies to employees, contractors, consultants, temporaries, and other workers at simfiny, Inc, including all
+personnel affiliated with third parties. This policy applies to all equipment that is owned or leased by simfiny, Inc.
 
 ## 4. Policy
 
 ### 4.1 General Use and Ownership
 
-**4.1.1** melodiy LLC proprietary information stored on electronic and computing devices whether owned or leased by
-melodiy LLC, the employee or a third party, remains the sole property of melodiy LLC. You must ensure through legal or
+**4.1.1** simfiny, Inc proprietary information stored on electronic and computing devices whether owned or leased by
+simfiny, Inc, the employee or a third party, remains the sole property of simfiny, Inc. You must ensure through legal or
 technical means that proprietary information is protected in accordance with the _Data Protection Standard_.
 
-**4.1.2** You have a responsibility to promptly report the theft, loss or unauthorized disclosure of melodiy LLC
+**4.1.2** You have a responsibility to promptly report the theft, loss or unauthorized disclosure of simfiny, Inc
 proprietary information.
 
-**4.1.3** You may access, use or share melodiy LLC proprietary information only to the extent it is authorized and
+**4.1.3** You may access, use or share simfiny, Inc proprietary information only to the extent it is authorized and
 necessary to fulfill your assigned job duties.
 
 **4.1.4** Employees are responsible for exercising good judgment regarding the reasonableness of personal use.
@@ -61,10 +60,10 @@ Individual departments are responsible for creating guidelines concerning person
 systems. In the absence of such policies, employees should be guided by departmental policies on personal use, and if
 there is any uncertainty, employees should consult their supervisor or manager.
 
-**4.1.5** For security and network maintenance purposes, authorized individuals within melodiy LLC may monitor equipment,
+**4.1.5** For security and network maintenance purposes, authorized individuals within simfiny, Inc may monitor equipment,
 systems and network traffic at any time, per Infosec's [_Audit Policy_](AUDIT_POLICY.md)
 
-**4.1.6** melodiy LLC reserves the right to audit networks and systems on a periodic basis to ensure compliance with
+**4.1.6** simfiny, Inc reserves the right to audit networks and systems on a periodic basis to ensure compliance with
 this policy.
 
 ### 4.2 Security and Proprietary Information
@@ -78,8 +77,8 @@ access to another individual, either deliberately or through failure to secure i
 **4.2.3** All computing devices must be secured with a password-protected screensaver with the automatic activation
 feature set to 10 minutes or less. You must lock the screen or log off when the device is unattended.
 
-**4.2.4** Postings by employees from a melodiy LLC email address to newsgroups should contain a disclaimer stating that
-the opinions expressed are strictly their own and not necessarily those of melodiy LLC, unless posting is in the course
+**4.2.4** Postings by employees from a simfiny, Inc email address to newsgroups should contain a disclaimer stating that
+the opinions expressed are strictly their own and not necessarily those of simfiny, Inc, unless posting is in the course
 of business duties.
 
 **4.2.5** Employees must use extreme caution when opening e-mail attachments received from unknown senders, which may
@@ -91,8 +90,8 @@ The following activities are, in general, prohibited. Employees may be exempted 
 course of their legitimate job responsibilities (e.g., systems administration staff may have a need to disable the
 network access of a host if that host is disrupting production services).
 
-Under no circumstances is an employee of melodiy LLC authorized to engage in any activity that is illegal under local,
-state, federal or international law while utilizing melodiy LLC-owned resources.
+Under no circumstances is an employee of simfiny, Inc authorized to engage in any activity that is illegal under local,
+state, federal or international law while utilizing simfiny, Inc-owned resources.
 
 The lists below are by no means exhaustive, but attempt to provide a framework for activities which fall into the
 category of unacceptable use.
@@ -103,11 +102,11 @@ The following activities are strictly prohibited, with no exceptions:
 
 1. Violations of the rights of any person or company protected by copyright, trade secret, patent or other intellectual
    property, or similar laws or regulations, including, but not limited to, the installation or distribution of
-   "pirated" or other software products that are not appropriately licensed for use by melodiy LLC.
+   "pirated" or other software products that are not appropriately licensed for use by simfiny, Inc.
 2. Unauthorized copying of copyrighted material including, but not limited to, digitization and distribution of
    photographs from magazines, books or other copyrighted sources, copyrighted music, and the installation of any
-   copyrighted software for which melodiy LLC or the end user does not have an active license is strictly prohibited.
-3. Accessing data, a server or an account for any purpose other than conducting melodiy LLC business, even if you have
+   copyrighted software for which simfiny, Inc or the end user does not have an active license is strictly prohibited.
+3. Accessing data, a server or an account for any purpose other than conducting simfiny, Inc business, even if you have
    authorized access, is prohibited.
 4. Exporting software, technical information, encryption software or technology, in violation of international or
    regional export control laws, is illegal. The appropriate management should be consulted prior to export of any
@@ -116,9 +115,9 @@ The following activities are strictly prohibited, with no exceptions:
    etc.).
 6. Revealing your account password to others or allowing use of your account by others. This includes family and other
    household members when work is being done at home.
-7. Using a melodiy LLC computing asset to actively engage in procuring or transmitting material that is in violation of
+7. Using a simfiny, Inc computing asset to actively engage in procuring or transmitting material that is in violation of
    sexual harassment or hostile workplace laws in the user's local jurisdiction.
-8. Making fraudulent offers of products, items, or services originating from any melodiy LLC account.
+8. Making fraudulent offers of products, items, or services originating from any simfiny, Inc account.
 9. Making statements about warranty, expressly or implied, unless it is a part of normal job duties.
 10. Effecting security breaches or disruptions of network communication. Security breaches include, but are not limited
     to, accessing data of which the employee is not an intended recipient or logging into a server or account that the
@@ -129,12 +128,12 @@ The following activities are strictly prohibited, with no exceptions:
 12. Executing any form of network monitoring which will intercept data not intended for the employee's host, unless this
     activity is a part of the employee's normal job/duty.
 13. Circumventing user authentication or security of any host, network or account.
-14. Introducing honeypots, honeynets, or similar technology on the melodiy LLC network.
+14. Introducing honeypots, honeynets, or similar technology on the simfiny, Inc network.
 15. Interfering with or denying service to any user other than the employee's host (for example, denial of service
     attack).
 16. Using any program/script/command, or sending messages of any kind, with the intent to interfere with, or disable, a
     user's terminal session, via any means, locally or via the Internet/Intranet/Extranet.
-17. Providing information about, or lists of, melodiy LLC employees to parties outside melodiy LLC.
+17. Providing information about, or lists of, simfiny, Inc employees to parties outside simfiny, Inc.
 
 #### 4.3.2 Email and Communication Activities
 
@@ -149,30 +148,30 @@ and not necessarily those of the company". Questions may be addressed to the IT 
 4. Solicitation of email for any other email address, other than that of the poster's account, with the intent to harass
    or to collect replies.
 5. Creating or forwarding "chain letters", "Ponzi" or other "pyramid" schemes of any type.
-6. Use of unsolicited email originating from within melodiy LLC's networks of other Internet/Intranet/Extranet service
-   providers on behalf of, or to advertise, any service hosted by melodiy LLC or connected via melodiy LLC's network.
+6. Use of unsolicited email originating from within simfiny, Inc's networks of other Internet/Intranet/Extranet service
+   providers on behalf of, or to advertise, any service hosted by simfiny, Inc or connected via simfiny, Inc's network.
 7. Posting the same or similar non-business-related messages to large numbers of Usenet newsgroups (newsgroup spam).
 
 #### 4.3.3 Blogging and Social Media
 
-1. Blogging by employees, whether using melodiy LLC’s property and systems or personal computer systems, is also subject
-   to the terms and restrictions set forth in this Policy. Limited and occasional use of melodiy LLC’s systems to engage
+1. Blogging by employees, whether using simfiny, Inc property and systems or personal computer systems, is also subject
+   to the terms and restrictions set forth in this Policy. Limited and occasional use of simfiny, Inc systems to engage
    in blogging is acceptable, provided that it is done in a professional and responsible manner, does not otherwise violate
-   melodiy LLC’s policy, is not detrimental to melodiy LLC’s best interests, and does not interfere with an employee's
-   regular work duties. Blogging from melodiy LLC’s systems is also subject to monitoring.
-2. melodiy LLC’s Confidential Information policy also applies to blogging. As such, Employees are prohibited from
-   revealing any melodiy LLC confidential or proprietary information, trade secrets or any other material covered by
-   melodiy LLC’s Confidential Information policy when engaged in blogging.
+   simfiny, Inc policy, is not detrimental to simfiny, Inc best interests, and does not interfere with an employee's
+   regular work duties. Blogging from simfiny, Inc systems is also subject to monitoring.
+2. simfiny, Inc Confidential Information policy also applies to blogging. As such, Employees are prohibited from
+   revealing any simfiny, Inc confidential or proprietary information, trade secrets or any other material covered by
+   simfiny, Inc Confidential Information policy when engaged in blogging.
 3. Employees shall not engage in any blogging that may harm or tarnish the image, reputation and/or goodwill of melodiy
    LLC and/or any of its employees. Employees are also prohibited from making any discriminatory, disparaging,
-   defamatory or harassing comments when blogging or otherwise engaging in any conduct prohibited by melodiy LLC’s
+   defamatory or harassing comments when blogging or otherwise engaging in any conduct prohibited by simfiny, Inc
    _Non-Discrimination_ and _Anti-Harassment_ policy.
-4. Employees may also not attribute personal statements, opinions or beliefs to melodiy LLC when engaged in blogging. If
+4. Employees may also not attribute personal statements, opinions or beliefs to simfiny, Inc when engaged in blogging. If
    an employee is expressing his or her beliefs and/or opinions in blogs, the employee may not, expressly or implicitly,
-   represent themselves as an employee or representative of melodiy LLC. Employees assume any and all risk associated with
+   represent themselves as an employee or representative of simfiny, Inc. Employees assume any and all risk associated with
    blogging.
 5. Apart from following all laws pertaining to the handling and disclosure of copyrighted or export controlled
-   materials, melodiy LLC’s trademarks, logos and any other melodiy LLC intellectual property may also not be used in
+   materials, simfiny, Inc trademarks, logos and any other simfiny, Inc intellectual property may also not be used in
    connection with any blogging activity
 
 ## 5. Policy Compliance
